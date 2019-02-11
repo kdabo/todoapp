9b1bd97eca8a227a61c062ed081419ce79683bc7
katarina <katarina.dabo@gmail.com>
2019-02-11 21:26:55 +0100

    remove idea

--- src/env.js
+++
@@ -1,6 +0,0 @@
-export const FIREBASE_API_KEY='AIzaSyB6D2xNRhQpLKisXRC3wGZjTVJvMW427Wo';
-export const FIREBASE_AUTH_DOMAIN='todoapp-be29e.firebaseapp.com';
-export const FIREBASE_DATABASE_URL='https://todoapp-be29e.firebaseio.com';
-export const FIREBASE_PROJECT_ID='todoapp-be29e';
-export const FIREBASE_STORAGE_BUCKET='';
-export const FIREBASE_MESSAGING_SENDER_ID='824056647393';
